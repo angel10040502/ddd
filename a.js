c44e82deef178232cdf6733b5ac5e1e1b056b9c8
angel10040502 <ntro.fans@gmail.com>
2026-01-21 02:54:15 +0900

    aaa

--- a.js
+++ a.js
@@ -5,13 +5,7 @@ for (int i = 0; i < 10; i++) {
     console.log("hello world");
     console.log("hello world");
     console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
-    console.log("hello world");
+    console.log("hello world");z
 
     
 
